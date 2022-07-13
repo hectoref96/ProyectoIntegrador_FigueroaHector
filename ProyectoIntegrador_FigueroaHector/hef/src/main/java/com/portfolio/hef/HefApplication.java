dae9d32b3f87e06deefd514d2f7c5b4cb9568afe
hectoref96 <fhectoremanuel@gmail.com>
2022-07-12 21:12:17 -0300

    backend

---
+++ ProyectoIntegrador_FigueroaHector/hef/src/main/java/com/portfolio/hef/HefApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.hef;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HefApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HefApplication.class, args);
+	}
+
+}
---
+++ ProyectoIntegrador_FigueroaHector/hef/src/test/java/com/portfolio/hef/HefApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.hef;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HefApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
